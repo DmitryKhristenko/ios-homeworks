de1a081d5ae24fd7eeb0021f835960a07e0dfc23
Dmitry Khristenko <dmitrykhrist@gmail.com>
2023-04-12 01:09:15 +0700

    resolving git conflicts

--- Navigation/Controllers/LoginViewController.swift
+++ Navigation/Controllers/LoginViewController.swift
@@ -8,9 +8,7 @@
 import UIKit
 
 final class LoginViewController: UIViewController {
-    
     private let loginScrollView = LoginScrollView()
-    
     override func loadView() {
         super.loadView()
         view = loginScrollView
@@ -23,24 +21,20 @@ final class LoginViewController: UIViewController {
             }
         }
     }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(changeDeviceOrientation), name: UIDevice.orientationDidChangeNotification, object: nil)
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         loginScrollView.loginScrollViewDelegate = self
     }
-    
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         changeDeviceOrientation()
     }
-    
     @objc private func changeDeviceOrientation() {
         // Update constraints based on device orientation
         if UIDevice.current.orientation.isPortrait {
@@ -51,33 +45,27 @@ final class LoginViewController: UIViewController {
             NSLayoutConstraint.activate(loginScrollView.landscapeConstraints)
         }
     }
-    
     @objc private func keyboardWillShow(notification: NSNotification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
             loginScrollView.scrollView.contentInset.bottom = keyboardSize.height + 50.0
             loginScrollView.scrollView.showsVerticalScrollIndicator = false
         }
     }
-    
     @objc private func keyboardWillHide() {
         loginScrollView.scrollView.contentInset = .zero
         loginScrollView.scrollView.verticalScrollIndicatorInsets = .zero
     }
-    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         NotificationCenter.default.removeObserver(UIResponder.keyboardWillShowNotification)
         NotificationCenter.default.removeObserver(UIResponder.keyboardWillHideNotification)
         NotificationCenter.default.removeObserver(UIDevice.orientationDidChangeNotification)
     }
-    
 }
 
-// MARK: - LoginScrollViewDelegate
 extension LoginViewController: LoginScrollViewDelegate {
     func pushProfileVC() {
         let profileViewController = ProfileViewController()
         navigationController?.pushViewController(profileViewController, animated: true)
     }
-    
 }
